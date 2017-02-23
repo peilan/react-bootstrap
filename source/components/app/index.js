a7c17ebc634a7609801b6d02c1b7bcfeab8ffe30
peilan <bl-74@yandex.ru>
2017-02-23 22:16:45 +0500

    test bootstrap page

--- source/components/app/index.js
+++ source/components/app/index.js
@@ -1,52 +1,19 @@
 import React, {Component} from 'react'
-import { Media, Image, Col, Grid, Row, FormGroup, FormControl,Button, InputGroup} from 'react-bootstrap';
+import { Jumbotron, Button } from 'react-bootstrap';
 import style from './style.css'
 
 export default class App extends Component {
   render() {
     return <div>
-    <Media>
-			<Grid>
-    		<Row className={style.margin}>
-					<Col xs={4} md={1}>
-						<Media.Left align="top">
-							<Image className={style.ava} src="./source/components/app/test.png" thumbnail circle />
-						</Media.Left>
-					</Col>
-					<Col xs={8} md={5}>
-						<Media.Body>
-							<Media.Heading>Top aligned media</Media.Heading>
-							<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-						</Media.Body>
-					</Col>
-				</Row>
-				<Row className={style.margin}>
-					<Col xs={4} md={1}>
-						<Media.Left align="top">
-							<Image className={style.ava} src="./source/components/app/test.png" thumbnail circle />
-						</Media.Left>
-					</Col>
-					<Col xs={8} md={5}>
-						<Media.Body>
-							<Media.Heading>Top aligned media</Media.Heading>
-							<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-						</Media.Body>
-					</Col>
-				</Row>
-				<Row>
-					<Col xs={8} md={5}>
-					  <FormGroup>
-							<InputGroup>
-								<FormControl type="text" />
-								<InputGroup.Button>
-									<Button>Action</Button>
-								</InputGroup.Button>
-							</InputGroup>
-						</FormGroup>
-					</Col>
-				</Row>
-			</Grid>
-    </Media>
+      <Jumbotron className={style.content}>
+        <h1>React-bootstrap example!</h1>
+        <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p><Button onClick={this.openDocs} bsStyle="primary">Learn more</Button></p>
+      </Jumbotron>
 		</div>
+  }
+
+  openDocs(){
+    location.href = 'https://react-bootstrap.github.io/components.html'
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,58 +2,69 @@ var path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const webpack = require('webpack')
 module.exports = {
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
-    path.resolve(__dirname, './source/index')
-  ],
+  entry: {
+    app: [ 'react-hot-loader/patch', 
+      'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000', 
+      path.resolve(__dirname, './source/index')
+    ],
+    vendor:[
+      'react',
+      'react-bootstrap',
+      'react-dom'
+    ]
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, './lib'),
-    publicPath: '/static/',
+    publicPath: '/static/'
   },
   devtool: 'inline-source-map',
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.js?$/,
         use: ['babel-loader']
       }, {
         test: /\.css$/,
         use: [
-          'style-loader',
-          {
+          'style-loader', {
             loader: 'css-loader',
             options: {
               modules: true,
-              localIdentName: '[path][name]__[local]',
+              localIdentName: '[path][name]__[local]'
             }
           }
         ]
-      },
-      {
+      }, {
         test: /\.html$/,
-        use: [{
-          loader: 'html-loader',
-          query: {
-            minimize: true
+        use: [
+          {
+            loader: 'html-loader',
+            query: {
+              minimize: true
+            }
           }
-        }]
-      },
-      {
+        ]
+      }, {
         test: /\.(png|jpg)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 15000
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 15000
+            }
           }
-        }]
+        ]
       }
     ]
   },
   plugins: [
     new webpack.NamedModulesPlugin(),
     new ExtractTextPlugin('bundle.css'),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor']
+    })
   ],
   stats: "errors-only"
 };
